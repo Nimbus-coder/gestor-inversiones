45638f429f7fb3f452db951bfbf0643ec2f87e34
Guido <135669743+Nimbus-coder@users.noreply.github.com>
2026-01-27 20:53:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,8 @@ with st.sidebar:
         del st.session_state['portfolio']
         st.rerun()
 
-    else: 
-    st.info("La Cartera está Vacía.")
+        else: 
+            st.info("La Cartera está Vacía.")
         
     st.divider()
     
@@ -136,6 +136,7 @@ if 'portfolio' in st.session_state and len(st.session_state['portfolio']) > 0:
 else:
 
     st.info("👈 Cargá tu primera acción en el menú de la izquierda para empezar.")
+
 
 
 
