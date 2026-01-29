328083d113606c077e5bd4914a78a114242c0bcc
Guido <135669743+Nimbus-coder@users.noreply.github.com>
2026-01-28 22:42:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ def obtener_dolares():
         # Consultamos la API que ya nos da todos los tipos de cambio
         response = requests.get("https://dolarapi.com/v1/dolares", timeout=10)
         datos = response.json()
-        st.writen(datos)
+        st.write(datos)
         
         # Transformamos la lista en un diccionario fácil de usar
         return {d['casa']: d['venta'] for d in datos}
         return precios
     except Exception as e:
-        st.error(f"Error al conectar  con la API de dólares: {e}")
+        st.error(f"Error al conectar  con la API de dólares: {e}") # Es mejor usar .error para que salga en rojo # O simplemente:
+        st.write(f"Error al conectar con la API de dólares: {e}")
         return None
 
 # --- PANEL DE COTIZACIONES ---
@@ -174,6 +175,7 @@ if hay_acciones or hay_bonos:
             st.info("Cargá un bono en la barra lateral para empezar.")
 else:
     st.info("👈 Cargá tu primer activo en la barra lateral para empezar.")
+
 
 
 
